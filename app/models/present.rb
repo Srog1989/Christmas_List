ad7b42da5bd746d2c2a2d7cda8939dcc3d00af92
Srog1989 <s.rogers0715@yahoo.com>
2020-12-12 20:13:46 -0700

    generated migrations

--- app/models/present.rb
+++ app/models/present.rb
@@ -1,3 +1,4 @@
 class Present < ApplicationRecord
-
+    belongs_to :user
+    belongs_to :recipient
 end
--- db/migrate/20201213022518_create_presents.rb
+++ db/migrate/20201213022518_create_presents.rb
@@ -1,15 +1,12 @@
 class CreatePresents < ActiveRecord::Migration[6.0]
   def change
     create_table :presents do |t|
-      t.srting :name
-      t.
-      *                 age_range: integer???
-      * 		category : string
-      *                 Brand: string
-      * 		user_id : integer
-      * 		recipeient_id : integer
-      * 		belongs_to :user
-      * 		belongs_to :recipient
+      t.string :name
+      t.string :category
+      t.string :brand
+      t.integer :cost
+      t.belongs_to :user
+      t.belongs_to :recipient
       t.timestamps
     end
   end
