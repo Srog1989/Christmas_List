c7016786ede11dfac122eada7c385faae580eb05
Srog1989 <s.rogers0715@yahoo.com>
2020-12-17 19:36:08 -0700

    added helper methods to application view and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
-    helpers do
+    helper_method :current_user
+
+    private
         
-        def current_user
-            binding.pry
-            User.find_by(id: session[:user_id])
-        end
+    def current_user
+        User.find_by(id: session[:user_id])
     end
+
+    def verified_user
+        redirect_to '/' unless user_is_authenticated
+    end
+
+    def user_is_authenticated
+        !!current_user
+    end
+
 end
