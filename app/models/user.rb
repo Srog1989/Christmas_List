caf9f2afa15e31008843367be9e01f6ba22756fa
Srog1989 <s.rogers0715@yahoo.com>
2020-12-17 19:14:35 -0700

    added link to logout in application view

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     has_many :presents
     has_many :recipients, through: :presents
     has_secure_password
-    validates :name, uniqueness: true, presence: true
+    validates :email, uniqueness: true, presence: true
 end
