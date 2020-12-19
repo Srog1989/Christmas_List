b4387c668bc26fe7ce932e14abe7ae7b340a3f77
Srog1989 <s.rogers0715@yahoo.com>
2020-12-18 22:11:47 -0700

    modified presents controller new create and show actions

--- app/controllers/presents_controller.rb
+++ app/controllers/presents_controller.rb
@@ -10,18 +10,26 @@ class PresentsController < ApplicationController
     end
     
     def create
-        #binding.pry
-        @present = current_user.present.build(present_params)
-        redirect_to new_present_path(@present)
+        # binding.pry
+        # @present = Present.new(present_params)
+        @present = current_user.presents.build(present_params)
+        if @present.save
+            redirect_to user_present_path(current_user, @present)
+        else
+            render :new
+        end
+        #redirect_to new_present_path(@present)
     end
 
     def show
-       
+       @present = Present.find_by(id: params[:id])
     end
+
+    private
 
 
     def present_params
-        params.require(:present).permit(:name, :brand, :cost, :user_id, :recipient_id, recipient_attributes: [:name, :email])
+        params.require(:present).permit(:name, :brand, :cost, :category,  :user_id, :recipient_id, recipient_attributes: [:email, :name])
     end
 end
 
