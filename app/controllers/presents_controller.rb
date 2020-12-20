a71ae616c9b13875277d3e78c2a7c10f1bb9e3e1
Srog1989 <s.rogers0715@yahoo.com>
2020-12-20 13:00:26 -0700

    added edit and delete actions and views to present object

--- app/controllers/presents_controller.rb
+++ app/controllers/presents_controller.rb
@@ -23,11 +23,31 @@ class PresentsController < ApplicationController
        @present = Present.find_by(id: params[:id])
     end
 
+    def destroy
+        Present.find(params[:id]).destroy
+        redirect_to user_presents_path(current_user)
+    end
+
+    def edit
+        @present = Present.find(params[:id]) 
+    end
+
+      def update 
+        # binding.pry
+        @present = Present.find(params[:id])
+        updated = @present.update(present_params)
+        if updated
+          redirect_to present_path(@present)
+        else
+          render :edit
+        end
+      end
+
     private
 
 
     def present_params
-        params.require(:present).permit(:name, :brand, :cost, :category,  :user_id, :recipient_id, recipient_attributes: [:email, :name])
+        params.require(:present).permit(:name, :brand, :cost, :category,  :user_id, :recipient_id, recipient_attributes: [:email, :name, :address, :favorite_color, :id])
     end
 end
 
