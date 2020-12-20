3a4ae819356f4d831216af81a269143c259a5fb5
Srog1989 <s.rogers0715@yahoo.com>
2020-12-19 21:18:27 -0700

    added validation for recipient model

--- app/models/present.rb
+++ app/models/present.rb
@@ -1,7 +1,7 @@
 class Present < ApplicationRecord
     belongs_to :user
     belongs_to :recipient
-    # accepts_nested_attributes_for :recipient
+    validates :name, :cost, :brand, :category, presence: true
 
     def recipient_attributes=(recipient_attributes)
         recipient = Recipient.find_or_create_by(recipient_attributes)
--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -2,5 +2,5 @@ class Recipient < ApplicationRecord
     has_many :presents
     has_many :users, through: :presents
     validates :email, uniqueness: true, presence: true
-    
+    validates :name, presence: true
 end
