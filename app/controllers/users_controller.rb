7911c7158be4609c05f44cb339d42590df375fb9
Srog1989 <s.rogers0715@yahoo.com>
2020-12-19 21:16:26 -0700

    refactored users controller create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
    
 
     def new
-        @user = current_user
+        @user = User.new
     end
 
     def show
@@ -12,9 +12,10 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             redirect_to user_recipients_path(@user)
         else
-            render user_path
+            render :new
         end
     end
 
