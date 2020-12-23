424a0bcf602b127cc379f4abc8ff26fb3551dc4d
Srog1989 <s.rogers0715@yahoo.com>
2020-12-23 12:39:47 -0700

    added destroy action to recipients controller

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -1,5 +1,7 @@
 class RecipientsController < ApplicationController
     before_action :verified_user
+    before_action :set_recipient, only: [:show, :edit, :update, :destroy]
+    # before_action :redirect_if_not_authorized, only: [:edit, :update, :destroy]
 
     def index
         @recipients = current_user.recipients.order_recipients_by
@@ -20,15 +22,15 @@ class RecipientsController < ApplicationController
     end
 
     def show
-        @recipient = Recipient.find_by(id: params[:id])
+        # @recipient = Recipient.find_by(id: params[:id])
     end
 
     def edit
-        @recipient = Recipient.find(params[:id]) 
+        # @recipient = Recipient.find(params[:id]) 
     end
 
     def update
-            @recipient = Recipient.find(params[:id])
+            # @recipient = Recipient.find(params[:id])
             updated = @recipient.update(recipient_params)
         if updated
             redirect_to recipient_path(@recipient)
@@ -37,9 +39,27 @@ class RecipientsController < ApplicationController
         end
     end
 
+    def destroy
+        @recipient.destroy
+        redirect_to recipient_path
+
+    end
+
     private
 
     def recipient_params
         params.require(:recipient).permit(:name, :email, :address, :favorite_color)
     end
+
+    def set_recipient
+        @recipient = Recipient.find_by(id: params[:id])
+        if !@recipient
+            flash[:message] = "Recipient was not found."
+             redirect_to recipients_path
+        end
+    end
+
+    # def redirect_if_not_authorized
+    #     redirect_to recipients_path if @recipient.user != current_user
+    # end
 end
