e31d8b8af40726acc16d9889438d647513da8107
Srog1989 <s.rogers0715@yahoo.com>
2020-12-25 20:54:15 -0700

    modified recipients controller

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -22,15 +22,12 @@ class RecipientsController < ApplicationController
     end
 
     def show
-        # @recipient = Recipient.find_by(id: params[:id])
     end
 
     def edit
-        # @recipient = Recipient.find(params[:id]) 
     end
 
     def update
-            # @recipient = Recipient.find(params[:id])
             updated = @recipient.update(recipient_params)
         if updated
             redirect_to recipient_path(@recipient)
